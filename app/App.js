dcb8bf0a810e046610143d0c45a190a899ba54e9
TheActuallyRealDavid <davidfonteneau22@gmail.com>
2025-02-06 15:19:12 -0500

    example

--- app/App.js
+++ app/App.js
@@ -3,12 +3,12 @@ import { View, Text, SafeAreaView } from 'react-native'
 
 const App = () => {
     return (
-        <SafeAreaView>
-            <View style={{backgroundColor: 'pink'}}>
-                <Text>Current Weather</Text>
-            </View>
-        </SafeAreaView>
-    )
+      <SafeAreaView>
+        <View style={{ backgroundColor: "pink" }}>
+          <Text>Current Weather</Text>
+        </View>
+      </SafeAreaView>
+    );
 }
 
 export default App;
