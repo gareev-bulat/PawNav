c5ce2674ec3dc629bbe6d107917ce34bfc16bcf6
Булат Гареев <bulatgareev@n3-26-217.dhcp.drexel.edu>
2025-03-06 15:22:20 -0500

    commit

--- app/src/components/Maps.js
+++ app/src/components/Maps.js
@@ -1,19 +1,56 @@
 import React, { useRef, useEffect, useContext } from 'react';
-import { View, StyleSheet, Text, Image, TouchableOpacity, Alert } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import { View, StyleSheet, Text, Image} from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout, CalloutSubview } from "react-native-maps";
 import { MapPrompts } from './MapPrompts';
 import DataBase from '../utilities/data';
+import * as Constants from '../utilities/constants';
 
 
 const styles = StyleSheet.create({
   markerImage: {
     backgroundColor: 'white',
     borderRadius: 20,
-    borderWidth: 5,
-    borderColor: 'white',
+    borderWidth: 0.5,
+    borderColor: Constants.DARK_RED,
+    padding: 5,
     borderBottomRightRadius: 0.1,
     width: 45,
     height: 45,
+  },
+
+  infoWindow: {
+    width: 155,
+    height: 105,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 5,
+    marginBottom: 5,
+    borderColor: Constants.DARK_RED,
+    borderWidth: 0.5,
+  },
+
+  headerText: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+
+  subTitle: {
+    paddingTop: 5,
+    paddingLeft: 5,
+    textAlign: 'left',
+    fontSize: 12,
+    fontWeight: '700',
+    
+  },
+
+  bodyText: {
+    paddingLeft: 5,
+    paddingTop: 3,
+    textAlign: 'left',
+    fontSize: 12,
+    fontWeight: 'normal',
   },
 
   container: {
@@ -26,24 +63,37 @@ const styles = StyleSheet.create({
   },
 });
 
-const Markers = DataBase.map(({location, name, id}) => {
+//Markers Render
+const Markers = DataBase.map(({location, name, id, workHours, link}) => {
   return (
-    <Marker key={id} coordinate={location} onPress={() => alert(name)}>
+    <Marker key={id} coordinate={location}>
       <Image
-            source={require('../../assets/images/Menu_icon.png')}
+            source={require('../../assets/images/Marker_icon.png')}
             style={styles.markerImage}
             resizeMode="contain"
           />
+      <Callout tooltip={true}>
+        <View style={styles.infoWindow}>
+          <Text style={styles.headerText}>{name}</Text>
+          <Text style={styles.subTitle}>Hours:</Text>
+          <Text style={styles.bodyText}>{workHours}</Text>
+          <Text style={styles.subTitle}>Website:</Text>
+          <Text style={styles.bodyText}>{link}</Text>
+        </View>
+      </Callout>
     </Marker>
   )
 });
+////////////////////
 
 const Maps = () => {
   const { prompts } = useContext(MapPrompts);
   const mapReference = useRef(null);
+  
   console.log("user-location", prompts.UserRegion)
+  console.log("destination", prompts.DestinationRegion)
 
-  const mapRegion = prompts.DestinationRegion && prompts.DestinationRegion.latitude
+  const mapRegion = prompts.DestinationRegion.latitude
       ? prompts.DestinationRegion
       : prompts.UserRegion;
 
@@ -63,10 +113,16 @@ const Maps = () => {
   };
 
   useEffect(() => {
-    if (prompts.DestinationRegion && prompts.DestinationRegion.latitude) {
+    if (prompts.DestinationRegion.latitude) {
       animateToDestination(prompts.DestinationRegion);
     }
   }, [prompts.DestinationRegion]);
+
+  useEffect(() => {
+    if (prompts.UserRegion.latitude) {
+      animateToDestination(prompts.UserRegion);
+    }
+  }, [prompts.UserRegion]);
 
   return (
     <View style={styles.container}>
@@ -75,22 +131,22 @@ const Maps = () => {
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={mapRegion}
+        customMapStyle={[
+          {
+            featureType: 'poi',
+            elementType: 'labels',
+            stylers: [{ visibility: 'off' }],
+          },
+          {
+            featureType: 'transit',
+            elementType: 'labels',
+            stylers: [{ visibility: 'off' }],
+          },
+        ]}
         showsUserLocation={true}
       >
         {Markers}
       </MapView>
-      
-       
-        {/*{prompts.DestinationRegion && prompts.DestinationRegion.latitude && (
-          <Marker coordinate={prompts.DestinationRegion} onPress={() => alert(prompts.Name)}>
-            <Image
-            source={require('../../assets/images/Menu_icon.png')}
-            style={styles.markerImage}
-            resizeMode="contain"
-          />
-          </Marker>
-        )}*/}
-      
     </View>
   );
 };
